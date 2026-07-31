2eaa9edadcb7532358d939005d44a11f26f50126
Siraj Tariq <sirajtariq0@gmail.com>
2026-07-31 15:28:20 +0500

    increase the token expiry

--- erp_backend/settings.py
+++ erp_backend/settings.py
@@ -111,8 +111,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
 # ---------------------------------------------------------------------------
